d395ceaecc04ec795200d9f64630fcc4f76f19c6
Tony Bowen <wbowen30@gatech.edu>
2021-01-11 15:02:57 -0500

    qwerasdf

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -53,7 +53,7 @@ public class Main extends ListenerAdapter{
 
   public static void main(String[] args) throws Exception {
     SpringApplication.run(Main.class, args);
-    JDABuilder.createLight("Nzk2NTIxNTE3Mzk5NjcwODA0.X_ZIeA.iNHyE3NsSRQB9VxX1_Eb_7qC6Cg", GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
+    JDABuilder.createLight("Nzk2NTIxNTE3Mzk5NjcwODA0.X_ZIeA.1lZ_1jvoZ03P53P1ANUsuHvwMC4", GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
             .addEventListeners(new Main())
             .build();
   }
